c8473e39ced95e2f53bd41ce1a88a374fbbeff83
simplay <silent.simplay@gmail.com>
2018-11-29 00:11:00 +0100

    Write bmp image

--- app/main.cpp
+++ app/main.cpp
@@ -3,15 +3,78 @@
 
 #include "exampleConfig.h"
 #include "point3f.h"
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-  std::cout << "C++ Raytracer v"
+int main(int argc, char *argv[]) {
+    std::cout << "C++ Raytracer v"
             << PROJECT_VERSION_MAJOR
             << "."
             << PROJECT_VERSION_MINOR
             << std::endl;
 
-  Point3f d = Point3f(5, 1, 3);
-  std::cout << "Dot product is:" << d.dot() << std::endl;
-  return 0;
+    int w = 100;
+    int h = 100;
+    int x;
+    int y;
+    int r;
+    int g;
+    int b;
+
+    FILE *f;
+    unsigned char *img = NULL;
+    int filesize = 54 + 3 * w * h;
+
+    img = (unsigned char *)malloc(3 * w * h);
+    memset(img, 0, 3 * w * h);
+
+    for (int i = 0; i < w; i++) {
+      for (int j = 0; j < h; j++) {
+        x = i;
+        y = (h - 1) - j;
+        r = 1 * 255;
+        g = 0 * 255;
+        b = 0 * 255;
+        if (r > 255)
+          r = 255;
+        if (g > 255)
+          g = 255;
+        if (b > 255)
+          b = 255;
+        img[(x + y * w) * 3 + 2] = (unsigned char)(r);
+        img[(x + y * w) * 3 + 1] = (unsigned char)(g);
+        img[(x + y * w) * 3 + 0] = (unsigned char)(b);
+      }
+    }
+
+    unsigned char bmpfileheader[14] = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
+    unsigned char bmpinfoheader[40] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0};
+    unsigned char bmppad[3] = {0, 0, 0};
+
+    bmpfileheader[2] = (unsigned char)(filesize);
+    bmpfileheader[3] = (unsigned char)(filesize >> 8);
+    bmpfileheader[4] = (unsigned char)(filesize >> 16);
+    bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+    bmpinfoheader[4] = (unsigned char)(w);
+    bmpinfoheader[5] = (unsigned char)(w >> 8);
+    bmpinfoheader[6] = (unsigned char)(w >> 16);
+    bmpinfoheader[7] = (unsigned char)(w >> 24);
+    bmpinfoheader[8] = (unsigned char)(h);
+    bmpinfoheader[9] = (unsigned char)(h >> 8);
+    bmpinfoheader[10] = (unsigned char)(h >> 16);
+    bmpinfoheader[11] = (unsigned char)(h >> 24);
+
+    f = fopen("img.bmp", "wb");
+    fwrite(bmpfileheader, 1, 14, f);
+    fwrite(bmpinfoheader, 1, 40, f);
+    for (int i = 0; i < h; i++) {
+      fwrite(img + (w * (h - i - 1) * 3), 3, w, f);
+      fwrite(bmppad, 1, (4 - (w * 3) % 4) % 4, f);
+    }
+
+    free(img);
+    fclose(f);
+
+    return 0;
 }
