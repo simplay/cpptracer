30c9c83c81e9a09abaa83a8d70336a0f146ffc35
simplay <silent.simplay@gmail.com>
2019-01-05 03:45:02 +0100

    Partition render tasks

--- app/main.cpp
+++ app/main.cpp
@@ -6,41 +6,88 @@
 #include <string.h>
 #include <image.h>
 #include <thread>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-void doSomething(int id) {
-    cout << "Thread id = " << id << endl;
+struct render_task {
+  vector<int> indices;
+  int width;
+  int height;
+  // Scene scene;
+};
+
+void computeContribution(int id, render_task renderTask) {
+  for (vector<int>::iterator it = renderTask.indices.begin(); it != renderTask.indices.end(); ++it) {
+    // 2d image coordinates
+    int ii = *it / renderTask.width;
+    int jj = *it % renderTask.height;
+
+    // TODO compute contribution
+    // ray = renderTask.scene.camera.make_world_space_ray(ii, jj, samples[k-1])
+    // ray_spectrum = integrator.integrate(ray)
+
+    // write to film
+    // scene.film.add_sample(
+    //     ii + samples[k-1][0].to_f,
+    //     jj + samples[k-1][1].to_f,
+    //     ray_spectrum
+    //  )
+  }
 }
 
-/**
- * Spawns n threads
- */
-void spawnThreads(int n)
-{
+// spawns n threads
+void runRenderer(int n, int width, int height) {
     thread threads[n];
+    vector<int> indexLists[n];
+    vector<int> indexValues;
+
+    // initialize a vector that contains all 1d image coordinates
+    for (int i = 0; i < width * height; ++i) {
+      indexValues.push_back(i);
+    }
+
+    // permutate image coordiantes
+    random_shuffle(indexValues.begin(), indexValues.end());
+
+    // assign image coordinates to tasks
+    int counter = 0;
+    for (vector<int>::iterator it=indexValues.begin(); it != indexValues.end(); ++it) {
+      indexLists[counter % n].push_back(*it);
+      counter++;
+    }
+
     // spawn n threads:
     for (int i = 0; i < n; i++) {
-        threads[i] = thread(doSomething, i + 1);
+      render_task rt = render_task();
+      rt.width = width;
+      rt.height = height;
+      rt.indices = indexLists[i];
+      threads[i] = thread(computeContribution, i + 1, rt);
     }
 
     for (auto& th : threads) {
-        th.join();
+      th.join();
     }
+
+    Image img = Image(width, height);
+    img.print();
 }
 
 int main(int argc, char *argv[]) {
-    std::cout << "C++ Raytracer v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << std::endl;
+    cout << "C++ Raytracer v"
+         << PROJECT_VERSION_MAJOR
+         << "."
+         << PROJECT_VERSION_MINOR
+         << endl;
 
+    // don't hard-code these values, read from argv
+    int threadCount = 5;
+    int width = 500;
+    int height = 500;
 
-    Image img = Image(500, 500);
-    img.print();
-
-    spawnThreads(5);
+    runRenderer(threadCount, width, height);
 
     return 0;
 }
