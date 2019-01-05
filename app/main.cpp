f4afaf5ac6b58105a8ddeddf232b63ab53f66a4e
simplay <silent.simplay@gmail.com>
2019-01-05 02:44:52 +0100

    Prepare for running renderer in parallel

--- app/main.cpp
+++ app/main.cpp
@@ -5,6 +5,29 @@
 #include <stdio.h>
 #include <string.h>
 #include <image.h>
+#include <thread>
+
+using namespace std;
+
+void doSomething(int id) {
+    cout << "Thread id = " << id << endl;
+}
+
+/**
+ * Spawns n threads
+ */
+void spawnThreads(int n)
+{
+    thread threads[n];
+    // spawn n threads:
+    for (int i = 0; i < n; i++) {
+        threads[i] = thread(doSomething, i + 1);
+    }
+
+    for (auto& th : threads) {
+        th.join();
+    }
+}
 
 int main(int argc, char *argv[]) {
     std::cout << "C++ Raytracer v"
@@ -13,8 +36,11 @@ int main(int argc, char *argv[]) {
             << PROJECT_VERSION_MINOR
             << std::endl;
 
+
     Image img = Image(500, 500);
     img.print();
+
+    spawnThreads(5);
 
     return 0;
 }
