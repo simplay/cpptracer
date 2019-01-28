8d54382deef48a98cea7dc76bc5e95b05bf29c70
simplay <silent.simplay@gmail.com>
2019-01-28 22:47:10 +0100

    Add new scene that illustrates an explosion

---
+++ src/scenes/explosionTest.cpp
@@ -0,0 +1,29 @@
+#include "explosionTest.h"
+#include "../intersectables/plane.h"
+#include "../intersectables/sphere.h"
+#include "../intersectables/explosion.h"
+#include "../materials/blinn.h"
+#include "../materials/diffuse.h"
+
+ExplosionTest::ExplosionTest(int width, int height)
+  : Scene(width, height) {}
+
+  void ExplosionTest::buildLights() {
+    std::vector<PointLight*>* lightList = new std::vector<PointLight*>;
+    lightList->push_back(new PointLight(new Point3f(0.5, 0.5, 2.0), new Spectrum(1.0)));
+    lightList->push_back(new PointLight(new Point3f(-0.75, 0.75, 2.0), new Spectrum(1.0, 0.0, 10.0)));
+    this->lightList = lightList;
+  }
+
+void ExplosionTest::buildIntersectables() {
+  IntersectableList* intersectableList = new IntersectableList();
+  Blinn* material = new Blinn(new Spectrum(1.0, 0.0, 0.0), new Spectrum(0.6), 50.0);
+  Material* diffuse = new Diffuse(new Spectrum(0.0, 0.5, 0.5));
+  intersectableList->put(new Explosion(material, new Point3f(0.0, 0.0, 0.0), 1.5));
+  intersectableList->put(new Plane(diffuse, new Point3f(1.0, 0.0, 0.0), 1));
+  intersectableList->put(new Plane(diffuse, new Point3f(-1.0, 0.0, 0.0), 1));
+  intersectableList->put(new Plane(diffuse, new Point3f(0.0, 1.0, 0.0), 1));
+  intersectableList->put(new Plane(diffuse, new Point3f(0.0, -1.0, 0.0), 1));
+  intersectableList->put(new Plane(diffuse, new Point3f(0.0, 0.0, 1.0), 1));
+  this->intersectableList = intersectableList;
+}
