3165c1dd2584ad8f9cef5320b50688c03c164702
simplay <silent.simplay@gmail.com>
2019-02-01 01:12:55 +0100

    Implement first version of a intersectable triangle

---
+++ include/intersectables/triangle.h
@@ -0,0 +1,37 @@
+#include "intersectable.h"
+#include "../math/point3f.h"
+#include "../hitRecord.h"
+#include "../ray.h"
+
+class Material;
+
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+// Construct a plane given its normal and distance to the origin Note that the
+// distance is along the direction that the normal points (meaning that the
+// sign of distance matters)
+class Triangle : public Intersectable {
+  public:
+    Material* material;
+
+    // normal of the plane
+    Point3f* normal;
+
+    // normal of the plane
+    Point3f* a;
+    Point3f* b;
+    Point3f* c;
+
+
+    Triangle(Material*, Point3f*, Point3f*, Point3f*);
+    // plane-ray intersection ray: p(t) = orig + t * dir
+    //
+    // Implicit plane:
+    // f(p) = dot(n, (p - a)) intersection:
+    // f(p(t)) = 0. Solve for t.  Plug t_i into
+    // p(t_i) will give intersection point
+    virtual HitRecord* intersect(Ray* ray);
+};
+
+#endif
---
+++ src/intersectables/triangle.cpp
@@ -0,0 +1,59 @@
+#include "triangle.h"
+#include "matrix3f.h"
+
+Triangle::Triangle(Material* material, Point3f* a, Point3f* b, Point3f* c)
+  : material(material), a(a), b(b), c(c) {}
+
+HitRecord* Triangle::intersect(Ray* ray) {
+  Point3f* ab = new Point3f(a);
+  ab->sub(b);
+
+  Point3f* ac = new Point3f(a);
+  ac->sub(c);
+
+  Point3f* ao = new Point3f(a);
+  ao->sub(ray->origin);
+
+  Matrix3f m(
+    ab->x, ac->x, ray->direction->x,
+    ab->y, ac->y, ray->direction->y,
+    ab->z, ac->z, ray->direction->z
+  );
+  Matrix3f* invM = m.inv();
+  Point3f b(ao->x, ao->y, ao->z);
+
+  Point3f* params = invM->mult(&b);
+
+  // Perform conservative inside-outside check
+  if (params->x < 0 || params->x > 1) {
+    return new HitRecord();
+  }
+
+  if (params->y < 0 || params->y > 1) {
+    return new HitRecord();
+  }
+
+  // note that: alpha + beta + gamma = 1
+  if (params->x + params->y > 1) {
+    return new HitRecord();
+  }
+
+  float t = params->z;
+  Point3f* intersectionPosition = new Point3f(ray->direction);
+  intersectionPosition->scale(t);
+  intersectionPosition->add(ray->origin);
+
+  Point3f* wIn = new Point3f(ray->direction);
+  wIn->negate();
+  wIn->normalize();
+
+  return new HitRecord(
+    t,
+    intersectionPosition,
+    NULL, // TODO compute and set normal
+    NULL,
+    wIn,
+    material,
+    this
+  );
+}
