2db38b16df2b52937eef6556990265d54110bea3
simplay <silent.simplay@gmail.com>
2019-02-15 15:42:48 +0100

    Reduce noise in whitted integration
    Slightly perturbate ray-origins

--- src/ray.cpp
+++ src/ray.cpp
@@ -1,31 +1,32 @@
 #include "ray.h"
 #include "math/point3f.h"
 
+
 Ray::Ray()
   : origin(new Point3f()), direction(new Point3f()), depth(0), i(-1), j(-1) {}
 
 Ray::Ray(Point3f* origin, Point3f* direction)
-  : direction(direction), depth(0), i(-1), j(-1) {
+  : origin(origin), direction(direction), depth(0), i(-1), j(-1) {
   Point3f* o = new Point3f(direction);
   o->scale(0.00001);
   o->add(origin);
-  this->origin = origin;
+  this->origin = o;
 }
 
 Ray::Ray(Point3f* origin, Point3f* direction, int i, int j)
-  : direction(direction), depth(0), i(i), j(j) {
+  : origin(origin), direction(direction), depth(0), i(i), j(j) {
     Point3f* o = new Point3f(direction);
     o->scale(0.00001);
     o->add(origin);
-    this->origin = origin;
+    this->origin = o;
 }
 
 Ray::Ray(Point3f* origin, Point3f* direction, int depth)
-  : direction(direction), depth(depth), i(-1), j(-1) {
+  : origin(origin), direction(direction), depth(depth), i(-1), j(-1) {
   Point3f* o = new Point3f(direction);
   o->scale(0.00001);
   o->add(origin);
-  this->origin = origin;
+  this->origin = o;
 }
 
 Point3f* Ray::pointAt(float t) {
