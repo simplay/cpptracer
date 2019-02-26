cb538dd2fa201ee218a87f76a01bf67baa0145b0
simplay <silent.simplay@gmail.com>
2019-02-27 00:19:51 +0100

    Construct new Vector4f with a Point3f and a float

--- src/math/vector4f.cpp
+++ src/math/vector4f.cpp
@@ -1,10 +1,14 @@
 #include "vector4f.h"
+#include "point3f.h"
 
 Vector4f::Vector4f()
   : x(0), y(0), z(0), w(0) {}
 
 Vector4f::Vector4f(float x, float y, float z, float w)
   : x(x), y(y), z(z), w(w) {}
+
+Vector4f::Vector4f(Point3f* p, float w)
+  : x(p->x), y(p->y), z(p->z), w(w) {}
 
 float Vector4f::dot(Vector4f* other) {
   return x * other->x + y * other->y + z * other->z + w * other->w;
