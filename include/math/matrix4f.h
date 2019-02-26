a2d71e7fc1e6425a88427ec8efe3c701c19bce76
simplay <silent.simplay@gmail.com>
2019-02-27 00:20:35 +0100

    Construct new Matrix4f with 16 floats

--- include/math/matrix4f.h
+++ include/math/matrix4f.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "vector4f.h"
 
 #ifndef MATRIX4F_H
@@ -13,8 +14,19 @@ class Matrix4f {
 
   Matrix4f();
   Matrix4f(Vector4f* v1, Vector4f* v2, Vector4f* v3, Vector4f* v4, bool rowWise);
+  Matrix4f(
+    float m00, float m01, float m02, float m03,
+    float m10, float m11, float m12, float m13,
+    float m20, float m21, float m22, float m23,
+    float m30, float m31, float m32, float m33
+  );
 
   Vector4f* mult(Vector4f* other);
+
+  void log() {
+    printf("%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n",
+        m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33);
+  }
 };
 
 #endif
--- src/math/matrix4f.cpp
+++ src/math/matrix4f.cpp
@@ -8,6 +8,19 @@ Matrix4f::Matrix4f():
   m20(0), m21(0), m22(0), m23(0),
   m30(0), m31(0), m32(0), m33(0) {}
 
+
+  Matrix4f::Matrix4f(
+    float m00, float m01, float m02, float m03,
+    float m10, float m11, float m12, float m13,
+    float m20, float m21, float m22, float m23,
+    float m30, float m31, float m32, float m33
+  ):
+    m00(m00), m01(m01), m02(m02), m03(m03),
+    m10(m10), m11(m11), m12(m12), m13(m13),
+    m20(m20), m21(m21), m22(m22), m23(m23),
+    m30(m30), m31(m31), m32(m32), m33(m33) {}
+
+
 Matrix4f::Matrix4f(Vector4f* v1, Vector4f* v2, Vector4f* v3, Vector4f* v4, bool readRowWise) {
   if (readRowWise) {
     m00 = v1->x; m01 = v1->y; m02 = v1->z; m03 = v1->w;
