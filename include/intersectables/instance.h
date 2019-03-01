a50884c88bf0cb65a179ffd3946cf78314cd6ea4
simplay <silent.simplay@gmail.com>
2019-03-01 17:42:00 +0100

    Document instance header file

--- include/intersectables/instance.h
+++ include/intersectables/instance.h
@@ -9,17 +9,34 @@ class Material;
 #ifndef INSTANCE_H
 #define INSTANCE_H
 
+/**
+* Used to reduce memory usage: Instead re-creating very similar intersectables,
+* try to re-use them.
+*
+* An instance has a reference shadered intersectable and a homogeneous
+* tranformation which describes how an object is rotated, translated, scaled
+* and sheared in its using scene.
+*
+* An instance of an intersectable then never gets modified, only its
+* transformation matrix gets updated. This allows to save us from creating
+* many similar intersectable instances
+*/
 class Instance : public Intersectable {
   private:
+
+    // A reference to an intersectable, e.g. a sphere or a plane.
     Intersectable* intersectable;
 
-    // transformation matrix T
+    // homogeneous tranformation matrix T described in local object coordinate
+    // system, i.e. from object coordinates to world coords.
     Matrix4f* transformation;
 
-    // inverse transformation T^-1
+    // homogeneous inverse tranformation T^-1 described in world coordinate
+    // system, i.e. from world space coordinates to local coords.
     Matrix4f* invTransformation;
 
-    // denotes (T^-1)^t used to transform normals and tangent vectors
+    // homogeneous transposed inverse tranformation (T^-1)^t used to transform
+    // normals and tangent vectors, described in world coordinate system
     Matrix4f* invTrasnposedTransformation;
 
   public:
