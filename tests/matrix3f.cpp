9dda639ed148b3488dd53bbd4f576fc1c2381c63
simplay <silent.simplay@gmail.com>
2019-03-09 18:57:22 +0100

    Reduce memory leaks in matrix test classes

--- tests/matrix3f.cpp
+++ tests/matrix3f.cpp
@@ -4,34 +4,37 @@
 #include "math/vector3f.h"
 
 TEST(Matrix3f, mult) {
-  Matrix3f* mat = new Matrix3f(
+  Matrix3f mat(
     1, 2, 3,
     4, 5, 6,
-    7, 8, 9);
+    7, 8, 9
+  );
 
-  Vector3f* v = new Vector3f(1, 10, 100);
-  Vector3f* r = mat->mult(v);
-  ASSERT_EQ(321, r->x);
-  ASSERT_EQ(654, r->y);
-  ASSERT_EQ(987, r->z);
+  Vector3f v(1, 10, 100);
+  Vector3f r = mat.mult(&v);
+  ASSERT_EQ(321, r.x);
+  ASSERT_EQ(654, r.y);
+  ASSERT_EQ(987, r.z);
 }
 
 TEST(Matrix3f, det) {
-  Matrix3f* mat = new Matrix3f(
-      6, 1, 1,
-      4, -2, 5,
-      2, 8, 7);
+  Matrix3f mat = Matrix3f(
+    6, 1, 1,
+    4, -2, 5,
+    2, 8, 7
+  );
 
-  ASSERT_EQ(-306, mat->det());
+  ASSERT_EQ(-306, mat.det());
 }
 
 TEST(Matrix3f, inv) {
-  Matrix3f* mat = new Matrix3f(
-      -24, 18, 5,
-      20, -15, -4,
-      -5, 4, 1);
+  Matrix3f mat(
+    -24, 18, 5,
+    20, -15, -4,
+    -5, 4, 1
+  );
 
-  Matrix3f* m = mat->inv();
+  auto m = mat.inv();
 
   ASSERT_EQ(1, m->m00);
   ASSERT_EQ(2, m->m01);
@@ -44,6 +47,8 @@ TEST(Matrix3f, inv) {
   ASSERT_EQ(5, m->m20);
   ASSERT_EQ(6, m->m21);
   ASSERT_EQ(0, m->m22);
+
+  delete m;
 }
 
 // Data was produced via octave
@@ -62,23 +67,13 @@ TEST(Matrix3f, inv) {
 // 7.8429    1.4109   -2.0808
 TEST(Matrix3f, inv2) {
 
-  Matrix3f* mat = new Matrix3f(
+  Matrix3f mat(
     0.01022, 0.13142, -0.21362,
     -0.05683, -0.05683, 0.42147,
-    0.00000, 0.45682, -1.00000);
+    0.00000, 0.45682, -1.00000
+  );
 
-  Matrix3f* m = mat->inv();
-  // std::cout << m->m00 << std::endl;
-  // std::cout << m->m01 << std::endl;
-  // std::cout << m->m02 << std::endl;
-  //
-  // std::cout << m->m10 << std::endl;
-  // std::cout << m->m11 << std::endl;
-  // std::cout << m->m12 << std::endl;
-  //
-  // std::cout << m->m20 << std::endl;
-  // std::cout << m->m21 << std::endl;
-  // std::cout << m->m22 << std::endl;
+  auto m = mat.inv();
 
   float eps = 1e-2;
   ASSERT_NEAR(41.0024, m->m00, eps);
@@ -92,25 +87,28 @@ TEST(Matrix3f, inv2) {
   ASSERT_NEAR(7.8429, m->m20, eps);
   ASSERT_NEAR(1.4109, m->m21, eps);
   ASSERT_NEAR(-2.0808, m->m22, eps);
+
+  delete m;
 }
 
 TEST(Matrix3f, scale) {
-  Matrix3f* mat = new Matrix3f(
+  Matrix3f mat(
     1, 2, 3,
     4, 5, 6,
-    7, 8, 9);
+    7, 8, 9
+  );
 
-  mat->scale(10);
+  mat.scale(10);
 
-  ASSERT_EQ(10, mat->m00);
-  ASSERT_EQ(20, mat->m01);
-  ASSERT_EQ(30, mat->m02);
+  ASSERT_EQ(10, mat.m00);
+  ASSERT_EQ(20, mat.m01);
+  ASSERT_EQ(30, mat.m02);
 
-  ASSERT_EQ(40, mat->m10);
-  ASSERT_EQ(50, mat->m11);
-  ASSERT_EQ(60, mat->m12);
+  ASSERT_EQ(40, mat.m10);
+  ASSERT_EQ(50, mat.m11);
+  ASSERT_EQ(60, mat.m12);
 
-  ASSERT_EQ(70, mat->m20);
-  ASSERT_EQ(80, mat->m21);
-  ASSERT_EQ(90, mat->m22);
+  ASSERT_EQ(70, mat.m20);
+  ASSERT_EQ(80, mat.m21);
+  ASSERT_EQ(90, mat.m22);
 }
