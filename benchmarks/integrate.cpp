6e2fb5403dcc1106bae1d623df5a146579132b60
panmari <smmuzi@gmail.com>
2019-03-12 17:55:59 +0100

    Adding benchmark for integrate.
    For making this easier, making integrate class independent of scene,
    only passing light and intersectable list.

---
+++ benchmarks/integrate.cpp
@@ -0,0 +1,41 @@
+#include <benchmark/benchmark.h>
+#include "integrators/whittedIntegrator.h"
+#include "intersectables/sphere.h"
+#include "materials/diffuse.h"
+#include "materials/reflectiveMaterial.h"
+#include "materials/refractiveMaterial.h"
+
+namespace {
+std::vector<Ray> MakeRays() {
+  return {
+      Ray(new Vector3f(1.0, 1.0, 1.0), new Vector3f(-1, -1, -1)),
+      Ray(new Vector3f(-1.0, -1.0, -1.0), new Vector3f(1, 1, 1)),
+      Ray(new Vector3f(0.5, 0.5, 1.0), new Vector3f(0, 0, -1)),
+      Ray(new Vector3f(0.5, 0.5, -1.0), new Vector3f(0, 0, 1)),
+  };
+}
+
+static void BM_WhittedIntegrator(benchmark::State& state) {
+  Diffuse diffuse = Diffuse(new Spectrum(0.0, 0.5, 0.5));
+  RefractiveMaterial refractive = RefractiveMaterial(1.1);
+  ReflectiveMaterial reflective = ReflectiveMaterial();
+  IntersectableList* intersectableList = new IntersectableList();
+  intersectableList->put(new Sphere(&diffuse, Vector3f(0.0, 0.0, 0.0), 0.5));
+  intersectableList->put(new Sphere(&refractive, Vector3f(0.0, 0.8, 0.0), 0.5));
+  intersectableList->put(new Sphere(&reflective, Vector3f(0.8, 0.0, 0.0), 0.5));
+
+  std::vector<PointLight*>* lightList = new std::vector<PointLight*>;
+  lightList->push_back(new PointLight(Vector3f(0.5, 0.5, 2.0), new Spectrum(1.0)));
+
+  auto integrator = WhittedIntegrator(intersectableList, lightList);
+
+  auto rays = MakeRays();
+
+  for (auto _ : state) {
+    for (const auto& r : rays) {
+      benchmark::DoNotOptimize(integrator.integrate(r));
+    }
+  }
+}
+BENCHMARK(BM_WhittedIntegrator);
+}  // namespace
--- include/integrators/whittedIntegrator.h
+++ include/integrators/whittedIntegrator.h
@@ -8,7 +8,8 @@
 
 class WhittedIntegrator : public Integrator {
  private:
-  Scene* scene;
+  const IntersectableList* intersectableList;
+  const std::vector<PointLight*>* lights;
 
   // Check whether hitPosition receives light
   //
@@ -16,7 +17,7 @@ class WhittedIntegrator : public Integrator {
   // @param L light direction vector
   // @param t parameter of ray equation p_uvw(t) = 0 + t(s_uvw-0)
   // @return is light source occluded by object at hitPostion?
-  bool isOccluded(Vector3f* hitPosition, Vector3f* lightDir, float eps);
+  bool isOccluded(Vector3f* hitPosition, Vector3f* lightDir, float eps) const;
 
   // Compute BRDF contribution for a given source at closest intersection point.
   //
@@ -24,10 +25,11 @@ class WhittedIntegrator : public Integrator {
   // @param hitRecord closest intersection primary ray with scene.
   // @param t parameter of ray equation p_uvw(t) = 0 + t(s_uvw-0).
   // @return returns current spectrum of light source at intersaction point.
-  Spectrum* contributionOf(PointLight* lightSource, HitRecord* hitRecord);
+  Spectrum* contributionOf(const PointLight* lightSource, HitRecord* hitRecord) const;
 
  public:
-  WhittedIntegrator(Scene*);
+  WhittedIntegrator(const IntersectableList* intersectableList,
+                    const std::vector<PointLight*>* lights);
   Spectrum* integrate(const Ray&);
 };
 #endif
