4595cc511e47315c1a195130541384e10e0bfdbd
simplay <silent.simplay@gmail.com>
2019-04-13 13:11:48 +0200

    Move IntersectableList#getBoundingBox definition to source file

--- src/intersectables/intersectableList.cpp
+++ src/intersectables/intersectableList.cpp
@@ -32,4 +32,34 @@ HitRecord* IntersectableList::intersect(const Ray& ray) const {
 
   return hitRecord;
 }
+
+BoundingBox IntersectableList::getBoundingBox() const {
+  float minT = std::numeric_limits<float>::min();
+  float maxT = std::numeric_limits<float>::max();
+
+  auto minX = maxT;
+  auto minY = maxT;
+  auto minZ = maxT;
+
+  auto maxX = minT;
+  auto maxY = minT;
+  auto maxZ = minT;
+
+  for (auto const& intersectable : *container) {
+    auto boundingBox = intersectable->getBoundingBox();
+    auto bottomLeft = boundingBox.getBottomLeft();
+    auto topRight = boundingBox.getTopRight();
+
+    minX = std::min((double)bottomLeft.x, (double)minX);
+    minY = std::min((double)bottomLeft.y, (double)minY);
+    minZ = std::min((double)bottomLeft.z, (double)minZ);
+
+    maxX = std::max((double)topRight.x, (double)maxX);
+    maxY = std::max((double)topRight.y, (double)maxY);
+    maxZ = std::max((double)topRight.z, (double)maxZ);
+  }
+
+  return BoundingBox(Vector3f(minX, minY, minZ), Vector3f(maxX, maxY, maxZ));
+}
+
 void IntersectableList::put(Intersectable* intersectable) { container->push_back(intersectable); }
