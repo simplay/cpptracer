c348b3828090b29697f06a2877f0b7b0b8e7c6cd
simplay <silent.simplay@gmail.com>
2019-04-14 20:12:30 +0200

    Fix computation of BoundingBox for intersectableList

--- include/intersectables/boundingBox.h
+++ include/intersectables/boundingBox.h
@@ -1,6 +1,7 @@
 #ifndef BOUNDING_BOX_H
 #define BOUNDING_BOX_H
 
+#include <utility>
 #include "../hitRecord.h"
 #include "../ray.h"
 #include "intersectable.h"
@@ -29,7 +30,13 @@ class BoundingBox : public Intersectable {
 
  public:
   BoundingBox static buildFromVectors(const std::vector<Vector3f>& vectors);
-  BoundingBox& operator=(BoundingBox&) { return *this; }
+  BoundingBox& operator=(const BoundingBox& newBB) {
+    // This is a hack used to update certain const members whenever we put new
+    // members in IntersectableList.
+    *(Vector3f*)&bottomLeft = Vector3f(newBB.getBottomLeft());
+    *(Vector3f*)&topRight = Vector3f(newBB.getTopRight());
+    return *this;
+  }
 
   BoundingBox(const Vector3f& bottomLeft, const Vector3f& topRight);
   HitRecord* intersect(const Ray& ray) const;
--- tests/intersectableList.cpp
+++ tests/intersectableList.cpp
@@ -32,3 +32,60 @@ TEST(IntersectableList, getBoundingBox) {
   ASSERT_EQ(3, topRight.y);
   ASSERT_NEAR(3, topRight.z, 1e-5);
 }
+
+TEST(IntersectableList, put) {
+  auto intersectableList = IntersectableList();
+
+  auto s = Spectrum(0.0, 0.5, 0.5);
+  auto diffuse = Diffuse(&s);
+
+  auto tri = Triangle(1, &diffuse, Vector3f(0.0, 0.0, 0.0), Vector3f(1.0, 0.0, 0.0),
+      Vector3f(0.0, 1.0, 0.0));
+
+  auto tri2 = Triangle(2, &diffuse, Vector3f(3.0, 3.0, 3.0), Vector3f(1.0, 0.0, 0.0),
+      Vector3f(0.0, 1.0, -1.0));
+
+  intersectableList.getBoundingBox();
+
+  auto bb = intersectableList.getBoundingBox();
+
+  auto bottomLeft = bb.getBottomLeft();
+  auto topRight = bb.getTopRight();
+  ASSERT_EQ(0, bottomLeft.x);
+  ASSERT_EQ(0, bottomLeft.y);
+  ASSERT_EQ(0, bottomLeft.z);
+
+  ASSERT_EQ(0, topRight.x);
+  ASSERT_EQ(0, topRight.y);
+  ASSERT_NEAR(0, topRight.z, 1e-5);
+
+  intersectableList.put(&tri);
+  intersectableList.getBoundingBox();
+
+  bb = intersectableList.getBoundingBox();
+
+  bottomLeft = bb.getBottomLeft();
+  topRight = bb.getTopRight();
+  ASSERT_EQ(0, bottomLeft.x);
+  ASSERT_EQ(0, bottomLeft.y);
+  ASSERT_EQ(0, bottomLeft.z);
+
+  ASSERT_EQ(1, topRight.x);
+  ASSERT_EQ(1, topRight.y);
+  ASSERT_NEAR(0, topRight.z, 1e-5);
+
+  intersectableList.put(&tri2);
+  intersectableList.getBoundingBox();
+
+  bb = intersectableList.getBoundingBox();
+
+  bottomLeft = bb.getBottomLeft();
+  topRight = bb.getTopRight();
+  ASSERT_EQ(0, bottomLeft.x);
+  ASSERT_EQ(0, bottomLeft.y);
+  ASSERT_EQ(-1, bottomLeft.z);
+
+  ASSERT_EQ(3, topRight.x);
+  ASSERT_EQ(3, topRight.y);
+  ASSERT_NEAR(3, topRight.z, 1e-5);
+}
