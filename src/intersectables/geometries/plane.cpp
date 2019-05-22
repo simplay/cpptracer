56b29b880a27668bff6a4fca54b73172e84f0ec0
simplay <silent.simplay@gmail.com>
2019-05-22 22:29:51 +0200

    Workout docs for plane-ray intersection

--- src/intersectables/geometries/plane.cpp
+++ src/intersectables/geometries/plane.cpp
@@ -5,12 +5,15 @@ Plane::Plane(Material* material, const Vector3f& normal, float distance)
     : material(material), normal(normal), distance(distance), aabb(initBoundingBox()) {}
 
 /**
- * The distance D between a point p and a plane can be computed by:
- * D := dot((p - a), n) [0]
- * where n is a unit normal on the plane, and a is a reference point on the
- * plane
+ * In the following a derivation of the intersection formula we are using in
+ * this implementation to compute the plane ray intersection:
  *
- * Notice: dot((p - a), n) = 0 gives us all points p on the plane
+ * A plane can be defined by a normal n and a point p0 on the plane.
+ * Then, every other point p on the plane fulfills
+ *
+ * dot(p - p0, n) = 0
+ *
+ * This the implicit representation of a plane.
  *
  * A point on ray is given by the parametrization
  * r(t) = o + t*d [1] where
@@ -18,10 +21,39 @@ Plane::Plane(Material* material, const Vector3f& normal, float distance)
  * d := (dx, dy, dz) denotes the direction of the ray and
  * t is float-valued parameter
  *
- * We can compute the distance between a point on the ray and the plane by
- * setting p = r(t), which gives us: D = dot(o + t*d - a)
- * t = dot(a - o, n) / dot(d, n) and a = (0, 0, D)
+ * By setting p = r(t) and using the plane definition, we get
  *
+ * dot(r(t) - p0, n) = dot(o + t*d - p0, n) = 0
+ *
+ * <=>
+ *
+ * dot(t*d, n) + dot(o - p0, n) = 0
+ *
+ * because dot(t*d, n) = t*dot(d, n)
+ * =>
+ * t = -dot(o - p0, n) / dot(d, n)
+ *   = dot(p0 - o, n) / dot(d, n)
+ *
+ * which is the definition of the intersection parameter t.
+ *
+ * However, we can further simplify the formula of t when assuming[1]
+ *
+ * that p0 is defined as p0 = m * n, where m denotes a scalar.
+ * This is useful when we want to define a plane by a distance value m and a
+ * normal n
+ *
+ * then dot(p0 - o, n) = dot(m*n - o, n)
+ *                     = m * dot(n, n) - dot(o, n)
+ *                     = m * 1 - dot(o, n)
+ *                     = m - dot(o, n)
+ * and hence
+ * t = (m - dot(o, n)) / dot(d, n)
+ *
+ *
+ * Remarks:
+ *
+ * [1] : This can only be performed, when assuming, that the normal is
+ * always pointing outwards, i.e. away from the origin)
  */
 HitRecord* Plane::intersect(const Ray& ray) const {
   // incident angle: angle between the ray direction and the plane normal
@@ -73,7 +105,8 @@ std::vector<CsgSolid::IntervalBoundary> Plane::getIntervalBoundaries(const Ray& 
 
       // If the t value of the START boundary was positive, so is the t value
       // of the END boundary
-      b2.t = (hit->t > 0.0) ? std::numeric_limits<float>::max() : -std::numeric_limits<float>::max();
+      b2.t =
+          (hit->t > 0.0) ? std::numeric_limits<float>::max() : -std::numeric_limits<float>::max();
 
     } else {
       b1.type = BoundaryType::END;
@@ -81,7 +114,8 @@ std::vector<CsgSolid::IntervalBoundary> Plane::getIntervalBoundaries(const Ray& 
 
       // If the t value of the END boundary was positive, then the t value of
       // the START boundary is negative
-      b2.t = (hit->t > 0.0) ? -std::numeric_limits<float>::max() : std::numeric_limits<float>::max();
+      b2.t =
+          (hit->t > 0.0) ? -std::numeric_limits<float>::max() : std::numeric_limits<float>::max();
     }
 
     boundaries.push_back(b1);
